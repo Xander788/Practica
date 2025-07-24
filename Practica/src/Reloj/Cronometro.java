74180be6ce89dee2f93fadcf61c559bcdbdfde1e
Xander <pxander336@gmail.com>
2025-07-24 16:28:34 -0600

    Clase cronometro
    se crea la clase cronometro y sus metodos

---
+++ Practica/src/Reloj/Cronometro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reloj;
+
+/**
+ *
+ * @author Student
+ */
+public class Cronometro extends Tiempo implements Controlable {
+
+    @Override
+    public void iniciar() {
+        
+    }
+
+    @Override
+    public void detener() {
+        
+    }
+    
+    
+}
