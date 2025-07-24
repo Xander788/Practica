749eb4229639fdfdfcb751373e92b6d76608fd06
Brayan <brazofeifaam@est.utn.ac.cr>
2025-07-24 16:32:21 -0600

    Clase Reloj
    Se agrego una nueva función, en la clase como el método constructor

--- Practica/src/Reloj/Reloj.java
+++ Practica/src/Reloj/Reloj.java
@@ -10,6 +10,11 @@ package Reloj;
  */
 public class Reloj  extends Tiempo implements Configurable{
 
+    public Reloj() {
+        super();
+        this.activo=true;
+    }
+
     @Override
     public void configurar(int h, int m, int s) {
         this.hora=h;
