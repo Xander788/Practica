7a471be61021caed3a17318aa3a18456b85ab873
Brayan <brazofeifaam@est.utn.ac.cr>
2025-07-24 16:21:55 -0600

    Clase Reloj
    Se implemento la clase reloj

---
+++ Practica/src/Reloj/Reloj.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reloj;
+
+/**
+ *
+ * @author Braya
+ */
+public class Reloj  extends Tiempo implements Configurable{
+
+    @Override
+    public void configurar(int h, int m, int s) {
+        this.hora=h;
+        this.minutos=m;
+        this.segundos=s;
+    }
+}
