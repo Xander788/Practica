755d09655223df7e1748f4effdf14d5c89d1550b
Xander <pxander336@gmail.com>
2025-07-24 16:16:39 -0600

    Interface Configurable y Controlable
    Se crean los interface y sus metodos

--- Practica/src/Reloj/Controlable.java
+++ Practica/src/Reloj/Controlable.java
@@ -1,14 +1,14 @@
-package Reloj;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
+package Reloj;
 
 /**
  *
  * @author Student
  */
-public interface Configurable {
-    public void configurar(int h,int m,int s);
+public interface Controlable {
+    public void iniciar();
+    public void detener();
 }
